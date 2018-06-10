057b663671afee4753d5dcf543bb0cac2b9d0f80
Trần Đăng Khoa <tdangkhoa.itute@gmail.com>
2018-06-10 15:37:38 +0700

    Create Data Access Layer

---
+++ TeduShop.Data/Infrastructure/IDbFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TeduShop.Data.Infrastructure
+{
+    /// <summary>
+    /// Khởi tạo hàm Init()
+    /// </summary>
+    public interface IDbFactory:IDisposable
+    {
+        TeduShopDbContext Init();
+    }
+}
