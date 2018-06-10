d734f04297f481c8e7ec7bce3d85e0461604eade
Trần Đăng Khoa <tdangkhoa.itute@gmail.com>
2018-06-10 21:15:22 +0700

    add orther repository for tables

--- TeduShop.Data/Repositories/ProductCategoryRepository.cs
+++ TeduShop.Data/Repositories/ProductCategoryRepository.cs
@@ -5,28 +5,21 @@ using TeduShop.Model.Models;
 
 namespace TeduShop.Data.Repositories
 {
-    /// <summary>
-    /// Interface cuar riêng lớp ProductCategoryRepository
-    /// </summary>
-    public interface IProductCategoryRepository
+    public interface IProductCategoryRepository : IRepository<ProductCategory>
     {
-        IEnumerable<ProductCategory> GetCategoryByAlias(string alias);
+        IEnumerable<ProductCategory> GetByAlias(string alias);
     }
 
     public class ProductCategoryRepository : RepositoryBase<ProductCategory>, IProductCategoryRepository
     {
-        public ProductCategoryRepository(IDbFactory dbFactory) : base(dbFactory)
+        public ProductCategoryRepository(IDbFactory dbFactory)
+            : base(dbFactory)
         {
         }
 
-        /// <summary>
-        /// Implement interface
-        /// </summary>
-        /// <param name="alias"></param>
-        /// <returns></returns>
-        public IEnumerable<ProductCategory> GetCategoryByAlias(string alias)
+        public IEnumerable<ProductCategory> GetByAlias(string alias)
         {
-            return DbContext.ProductCategories.Where(x => x.Alias == alias);
+            return this.DbContext.ProductCategories.Where(x => x.Alias == alias);
         }
     }
 }
